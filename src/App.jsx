0344f3de7e033d103502bfbbbcae17d7b7b9cf9b
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-06-21 12:18:48 +0530

    complete app part-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import "./styles.css";
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Note from "./components/note";
+import Note from "./components/Note";
+import notes from "./components/notes";
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Note />
+
+      {notes.map((note) => (
+        <Note
+          id={note.key}
+          key={note.key}
+          title={note.title}
+          content={note.content}
+        />
+      ))}
+
       <Footer />
     </div>
   );
